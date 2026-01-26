4d55568a976d2f048d1c78d5965c705b65e4a958
kevin-nivek <kevinbotelhox@outlook.com>
2026-01-26 07:51:15 -0400

    criacao de pagina de login

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,24 @@
+import { Router } from '@angular/router';
+import { AuthFacade } from "../../core/auth/auth.facade";
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+@Component({
+  standalone: true,
+  selector: 'app-login',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.page.html',
+})
+export class LoginPage {
+
+  login = '';
+  senha = '';
+
+  constructor(private auth: AuthFacade, private router: Router) {}
+
+  entrar() {
+    this.auth.login(this.login, this.senha)
+    this.router.navigate(['/']);
+  }
+}
