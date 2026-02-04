c8550b73816da40cc20dfb8ef66e6f4b0532092a
kevin-nivek <kevinbotelhox@outlook.com>
2026-02-04 19:50:18 -0400

    ajuste de verificador de online

--- src/app/core/health/health.facade.ts
+++ src/app/core/health/health.facade.ts
@@ -1,6 +1,7 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { Inject, Injectable, PLATFORM_ID } from "@angular/core";
+import { BehaviorSubject, fromEvent, map, merge, of } from "rxjs";
 import { HealthService } from "./heallth.service";
+import { isPlatformBrowser } from "@angular/common";
 
 @Injectable({providedIn: 'root'})
 export class HealthFacade{
@@ -8,10 +9,33 @@ export class HealthFacade{
   status$ = this.statusSubject.asObservable();
 
   constructor(
-    private service: HealthService
-  ){}
+    private service: HealthService,
+     @Inject(PLATFORM_ID) private platformId: object
+  ){
+  if (isPlatformBrowser(this.platformId)) {
+      this.monitorConnection();
+    }
+  }
+
+  private monitorConnection(){
+    merge(
+      fromEvent(window, 'online').pipe(map(() => true)),
+      fromEvent(window, 'offline').pipe(map(() => false)),
+      of(navigator.onLine)
+    ).subscribe(isOnline => {
+      if (isOnline) {
+        this.checkHealth();
+      } else {
+        this.statusSubject.next('DOWN');
+      }
+    });
+  }
 
   checkHealth(){
+    if (!navigator.onLine) {
+      this.statusSubject.next('DOWN');
+      return;
+    }
     this.service.checkHealth().subscribe({
       next: () => this.statusSubject.next('UP'),
       error: err => {
@@ -22,5 +46,9 @@ export class HealthFacade{
         }
       }
     })
+  }
+
+  changeStatus(stats: 'DOWN' | 'UP'){
+    this.statusSubject.next(stats);
   }
 }
