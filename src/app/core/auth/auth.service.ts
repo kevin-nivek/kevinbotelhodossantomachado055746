c7e3b616634a373e9e426a579d53d50f6521ee95
kevin-nivek <kevinbotelhox@outlook.com>
2026-02-04 22:08:15 -0400

    ajsute verificador de localstore

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from "@angular/core";
+import { Inject, Injectable, PLATFORM_ID,  } from "@angular/core";
 import { enviroment } from "../../../enviroment/enviroment";
 import { HttpClient } from "@angular/common/http";
+import { isPlatformBrowser } from "@angular/common";
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ import { HttpClient } from "@angular/common/http";
 export class AuthService {
   private readonly baseUrl = `${enviroment.apiUrl}/autenticacao`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,@Inject(PLATFORM_ID) private platformId: Object) {}
 
   login(username: string, password: string) {
     return this.http.post<{ access_token: string , refresh_token: string, expires_in: number }>(`${this.baseUrl}/login`, { username, password });
@@ -21,6 +22,9 @@ export class AuthService {
   }
 
   isLogged(): boolean {
-    return !!localStorage.getItem('token')
+    if(isPlatformBrowser(this.platformId)){
+      return !!localStorage.getItem('token')
+    }
+    return false
   }
 }
